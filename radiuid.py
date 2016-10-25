5a6c9a8e60c707d016675f8bff78abe141c936bd
root <root@DEVEL.kernshosting.com>
2016-10-25 01:25:44 -0700

    Commit for v2.1.0

--- radiuid.py
+++ radiuid.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#####        RadiUID Server v2.0.1         #####
+#####        RadiUID Server v2.1.0         #####
 #####       Written by John W Kerns        #####
 #####      http://blog.packetsar.com       #####
 ##### https://github.com/PackeTsar/radiuid #####
