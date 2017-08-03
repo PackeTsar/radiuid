bbfbe498154a370f3cc0e9851364dc687ce78860
root <root@ip-172-31-32-73.us-east-2.compute.internal>
2017-08-03 17:30:59 -0400

    Issue #31 Fixed and Documented

--- radiuid.py
+++ radiuid.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#####        RadiUID Server v2.4.2         #####
+#####        RadiUID Server v2.4.3         #####
 #####       Written by John W Kerns        #####
 #####      http://blog.packetsar.com       #####
 ##### https://github.com/PackeTsar/radiuid #####
@@ -17,7 +17,7 @@ import platform
 import xml.etree.ElementTree as ElementTree
 
 ##### Inform RadiUID version here #####
-version = "2.4.2"
+version = "2.4.3"
 
 ##### Set some internal settings #####
 etcconfigfile = '/etc/radiuid/radiuid.conf'
